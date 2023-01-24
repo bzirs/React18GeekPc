b4dcb34341564083a3032e2d7f9f2c0767ac181f
bzirs <bzirs@outlook.com>
2023-01-24 22:34:43 +0800

    feat: ✨ 完成登录后token本地存储以及store从本地获取token等

--- src/store/reducers/token.js
+++ src/store/reducers/token.js
@@ -1,16 +1,22 @@
+import { getStorageToken, setStorageToken } from '@/utils/storage'
 import { createSlice } from '@reduxjs/toolkit'
+
+const authObj = JSON.parse(getStorageToken())
 
 const tokenSlice = createSlice({
   name: 'tokenSlice',
   initialState: {
-    token: '',
-    refresh_token: ''
+    token: authObj?.token || '',
+    refresh_token: authObj?.refresh_token || ''
   },
   reducers: {
     setToken: (state, { payload }) => {
       console.log(payload)
       state.token = payload.token
       state.refresh_token = payload.refresh_token
+
+      // 本地持久化
+      setStorageToken(payload)
     }
   }
 })
