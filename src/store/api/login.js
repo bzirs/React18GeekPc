f73882f80018445dc466b0fa849a95e68e22833c
bzirs <bzirs@outlook.com>
2023-01-25 12:01:55 +0800

    feat: ✨ 请求添加请求头

--- src/store/api/login.js
+++ src/store/api/login.js
@@ -1,10 +1,18 @@
 
+import { getStorageToken } from '@/utils/storage'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const loginApi = createApi({
   reducerPath: 'loginApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://geek.itheima.net'
+    baseUrl: 'http://geek.itheima.net',
+    prepareHeaders: headers => {
+      const token = JSON.parse(getStorageToken())?.token
+
+      // 添加请求头
+      token && headers.set('Authorization', `Bearer ${token}`)
+      return headers
+    }
   }),
   endpoints: bulid => ({
     login: bulid.mutation({
