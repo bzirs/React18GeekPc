e62224998a2d843e35a9f9c920c20825b297bf7b
bzirs <bzirs@outlook.com>
2023-01-25 16:47:16 +0800

    feat: ✨  组件切换中文

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+dayjs.locale('zh-cn')
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -13,7 +18,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+        <ConfigProvider locale={zhCN}>
         <App></App>
+        </ConfigProvider>
       </Router>
     </Provider>
   </React.StrictMode>
--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,5 +1,63 @@
+import React from 'react'
+import { Card, Breadcrumb, Form, Button, Radio, Select, DatePicker } from 'antd'
+import styles from './index.module.scss'
+import { Link } from 'react-router-dom'
+const { RangePicker } = DatePicker
+
 const Article = () => {
-  return (<div>Article</div>)
+  const handleChange = e => {
+    console.log(e)
+  }
+
+  return (
+    <div className={styles.root}>
+      <Card
+        title={
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link to='/'>首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>文章列表</Breadcrumb.Item>
+          </Breadcrumb>
+        }>
+        <Form>
+          <Form.Item label='状态'>
+            <Radio.Group>
+              <Radio value={-1}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label='频道'>
+            <Select
+              defaultValue='lucy'
+              style={{ width: 120 }}
+              onChange={handleChange}
+              options={[
+                { value: 'jack', label: 'Jack' },
+                { value: 'lucy', label: 'Lucy' },
+                { value: 'Yiminghe', label: 'yiminghe' },
+                { value: 'disabled', label: 'Disabled', disabled: true }
+              ]}
+            />
+          </Form.Item>
+
+          <Form.Item label='日期'>
+            <RangePicker />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type='primary' htmlType='submit'>
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Article
