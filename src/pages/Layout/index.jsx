af4de25483488b9cd2c6729bcef4d63c75fea46d
bzirs <bzirs@outlook.com>
2023-01-25 10:49:16 +0800

    feat: ✨ 首页布局

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -2,27 +2,18 @@ import React from 'react'
 import { Layout, Menu } from 'antd'
 
 // import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons'
-import {
-  HomeOutlined,
-  DiffOutlined,
-  EditOutlined, LogoutOutlined
-} from '@ant-design/icons'
+import { HomeOutlined, DiffOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons'
 import styles from './index.module.scss'
 import { SIDER_LIST } from '@/constant'
 // const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
 
 // 侧边栏
-const siderList = [
-  HomeOutlined,
-  DiffOutlined,
-  EditOutlined
-].map((t, i) => ({
+const siderList = [HomeOutlined, DiffOutlined, EditOutlined].map((t, i) => ({
   key: SIDER_LIST[i].id,
   icon: React.createElement(t),
   label: SIDER_LIST[i].label
-})
-)
+}))
 
 const MYLayout = () => {
   return (
@@ -40,19 +31,10 @@ const MYLayout = () => {
         </Header>
         <Layout>
           <Sider width={200} className='site-layout-background'>
-            <Menu theme='dark' mode='inline' items={siderList} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%', borderRight: 0 }}>
-            </Menu>
+            <Menu theme='dark' mode='inline' items={siderList} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%', borderRight: 0 }}></Menu>
           </Sider>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Content
-              className='site-layout-background'
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280
-              }}>
-              Content
-            </Content>
+          <Layout style={{ padding: '24px 24px' }}>
+            <Content className='site-layout-background'>我是内容</Content>
           </Layout>
         </Layout>
       </Layout>
