20cce625678a0902f2132f02b9a53fcdfadbecdb
bzirs <bzirs@outlook.com>
2023-01-26 17:28:22 +0800

    feat: ✨ 完成文章页上传图片

--- src/pages/Publish/index.jsx
+++ src/pages/Publish/index.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './index.module.scss'
-import { Card, Breadcrumb, Form, Input, Button, Space, Radio } from 'antd'
+import { Card, Breadcrumb, Form, Input, Button, Space, Radio, Upload } from 'antd'
 import { Link } from 'react-router-dom'
+import { PlusOutlined } from '@ant-design/icons'
 import ChannelList from '@/components/ChannelList'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { baseUrl } from '@/store/api/baseQuery'
 
 const Publish = props => {
-  const onChange = e => {
-    console.log(e)
+  // 图片链接列表
+  const [fileList, setFileList] = useState([
+  ])
+  // 控制type属性
+  const [type, setType] = useState(1)
+
+  // 存储图片列表
+  const fileRef = useRef(fileList)
+
+  const onTypeChange = e => {
+    const count = e.target.value
+    setType(count)
+    setFileList(fileRef.current.slice(0, count))
   }
-  const value = ''
+
+  const onChange = ({ fileList }) => {
+    setFileList(fileList)
+    fileRef.current = fileList
+  }
 
   return (
     <div className={styles.root}>
@@ -21,22 +40,32 @@ const Publish = props => {
             <Breadcrumb.Item>发布文章</Breadcrumb.Item>
           </Breadcrumb>
         }>
-        <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} size='large'>
-          <Form.Item label='标题'>
+        <Form initialValues={{ type: 1, channel_id: 0 }} labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} size='large'>
+          <Form.Item name='title' label='标题'>
             <Input placeholder='请输入文章的标题' style={{ width: 400 }}></Input>
           </Form.Item>
           {/* 频道 */}
           <ChannelList></ChannelList>
-          <Form.Item label='封面'>
-            <Radio.Group onChange={onChange} value={value}>
-              <Radio value={1}>A</Radio>
-              <Radio value={2}>B</Radio>
-              <Radio value={3}>C</Radio>
-              <Radio value={4}>D</Radio>
+          <Form.Item name='type' label='封面'>
+            <Radio.Group onChange={onTypeChange}>
+              <Radio value={1}>单图</Radio>
+              <Radio value={3}>三图</Radio>
+              <Radio value={0}>无图</Radio>
             </Radio.Group>
           </Form.Item>
-
-          <Form.Item label='内容'></Form.Item>
+          {fileList.length <= type && (
+            <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
+              {/* action: 上传的地址
+                name: 上传的文件的名字 默认file
+                fileList: 控制上传的图片,做回填 */}
+              <Upload action={`${baseUrl}/v1_0/upload`} maxCount={type} name='image' listType='picture-card' onChange={onChange} fileList={fileList}>
+                <PlusOutlined />
+              </Upload>
+            </Form.Item>
+          )}
+          <Form.Item name='content' label='内容'>
+            <ReactQuill></ReactQuill>
+          </Form.Item>
           <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
             <Space>
               <Button type='primary'>发布文章</Button>
--- src/store/api/baseQuery.js
+++ src/store/api/baseQuery.js
@@ -1,8 +1,10 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { getStorageToken } from '@/utils/storage'
 
+export const baseUrl = 'http://geek.itheima.net'
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://geek.itheima.net',
+  baseUrl,
   prepareHeaders: headers => {
     const token = JSON.parse(getStorageToken())?.token
     // 添加请求头
